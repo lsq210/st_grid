27408bb6a2c311349c9fceabc86ffd2c45920495
lsq210 <1135595767@qq.com>
2019-04-10 22:17:26 +0800

    读取 samples

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import os
+
 class Sample(object):
     id = -1
     date = None
@@ -14,9 +16,22 @@ class ST_GRID:
     k = 0
 
     @staticmethod
-    def load_samples(path):
-        pass
-    
+    def load_samples(folder):
+        all_files = os.listdir(folder)
+        files = [x for x in all_files if '.csv' in x]
+        index = 0
+        for file_name in files:
+            fp = open(folder + file_name, 'r')
+            raw_data = fp.readlines()
+            fp.close
+            for row in raw_data:
+                row_split = row.split()
+                print(row_split)
+                if row[3] == 0:
+                    ST_GRID.samples.append(Sample(index, row[0], row[1], row[5], row[4]))
+                    index += 1
+        print(ST_GRID.samples)
+
     @staticmethod
     def plot_k_dist(k):
         pass
---
+++ test.py
@@ -0,0 +1,3 @@
+from models import Sample, ST_GRID
+
+ST_GRID.load_samples('E:/教学资料/科研/样本数据/合并/')

a32b7c329bbca9e01be8eb07f27bea93dca73956
lsq210 <1135595767@qq.com>
2019-04-11 22:05:08 +0800

    按日期分组

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+from operator import itemgetter
+from itertools import groupby
 import os
 import math
+
 EARTH_RADIUS = 6371  # 地球平均半径，6371km
 C = 66 #时间间隔前的系数
 
@@ -19,22 +22,46 @@ class ST_GRID:
     k = 0
 
     @staticmethod
-    def load_samples(folder):
-        all_files = os.listdir(folder)
-        files = [x for x in all_files if '.csv' in x]
+    def init():
+        ST_GRID.samples = []
+        k = 0
+
+    @staticmethod
+    def load_samples(path):
         index = 0
-        for file_name in files:
-            fp = open(folder + file_name, 'r')
-            raw_data = fp.readlines()
-            fp.close
-            for row in raw_data:
-                row_split = row.split()
-                # print(row_split)
-                if row_split[3] == '0.0':
-                    ST_GRID.samples.append(Sample(index, row[0], row[1], row[5], row[4]))
-                    index += 1
-        # print(ST_GRID.samples)
+        file = open(path, 'r')
+        raw_data = file.readlines()
+        file.close
+        for row in raw_data:
+            row_split = row.split()
+            if row_split[3] == '0.0':
+                ST_GRID.samples.append(Sample(index, row_split[0], row_split[1], row_split[5], row_split[4]))
+                index += 1
+        ST_GRID.samples = ST_GRID.groupby(ST_GRID.samples)
     
+    @staticmethod
+    def groupby(samples):
+        groups = []
+        for sample in samples:
+            need_new_group = True
+            for group in groups:
+                if group[0].date == sample.date:
+                    group.append(sample)
+                    need_new_group = False
+            if need_new_group:
+                new_group = [sample,]
+                groups.append(new_group)
+        return groups
+    
+    @staticmethod
+    def print_samples():
+        for test in ST_GRID.samples:
+            for t in test[0:20]:
+                print(str(t.id) + ' ' + t.date + ' ' + str(t.lng) + ' ' + str(t.lat))
+            print('\n\n')
+        print('days {}'.format(len(ST_GRID.samples)))
+
+        
     def hav(theta):
         s = math.sin(theta / 2)
         return s * s
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from models import Sample, ST_GRID
+import os
 
-ST_GRID.load_samples('E:/教学资料/科研/样本数据/合并/')
+folder = 'E:/教学资料/科研/样本数据/合并/'
+all_files = os.listdir(folder)
+files = [x for x in all_files if '.csv' in x]
+for file_name in files:
+    ST_GRID.init()
+    ST_GRID.load_samples(folder + file_name)
